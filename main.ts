586bbbbf5b4f3b57686ee32f0c519f8752150234
ChaoticlyLowIQ <82467920+ChaoticlyLowIQ@users.noreply.github.com>
2021-04-26 19:04:01 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,26 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    if (voiceLine < 0) {
+        game.showLongText("This is just an ordinary Platformer", DialogLayout.Bottom)
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    voiceLine += 1
+})
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
     jumps = 0
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
-    currentlevel += 1
-    if (hasNextLevel()) {
-        pause(100)
-        game.splash("Next Level")
-        setLevelTileMap(currentlevel)
-    } else {
-        game.over(true, effects.melt)
-    }
 })
 function setLevelTileMap (num: number) {
     if (num == 0) {
         tiles.setTilemap(tilemap`level1`)
     } else if (num == 1) {
-    	
+        tiles.setTilemap(tilemap`level2`)
     } else if (num == 2) {
     	
     }
@@ -32,9 +37,20 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, l
     music.baDing.play()
     tiles.setTileAt(location, assets.tile`transparency16`)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
+    currentlevel += 1
+    if (hasNextLevel()) {
+        pause(100)
+        game.splash("Next Level")
+        setLevelTileMap(currentlevel)
+    } else {
+        game.over(true, effects.melt)
+    }
+})
 function hasNextLevel () {
     return currentlevel != levelcount
 }
+let voiceLine = 0
 let levelcount = 0
 let currentlevel = 0
 let jumps = 0
@@ -65,6 +81,7 @@ Jeff = sprites.create(img`
     ........................
     ........................
     `, SpriteKind.Player)
+Jeff.setPosition(15, 210)
 scene.setBackgroundColor(1)
 scene.cameraFollowSprite(Jeff)
 Jeff.ay = 300
@@ -72,6 +89,7 @@ jumps = 0
 currentlevel = 0
 levelcount = 3
 setLevelTileMap(currentlevel)
+voiceLine = 0
 game.onUpdate(function () {
     Jeff.x += controller.dx()
 })
